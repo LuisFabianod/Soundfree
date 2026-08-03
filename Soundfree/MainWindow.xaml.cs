19ef802122b2e424913766110687233435cd5c3e
Luis Fabiano <luisfabianodev@gmail.com>
2026-08-03 19:56:07 -0300

    Adicionar botão e mensagem

--- Soundfree/MainWindow.xaml.cs
+++ Soundfree/MainWindow.xaml.cs
@@ -11,14 +11,19 @@ using System.Windows.Shapes;
 
 namespace Soundfree
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+ 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Você clicou no botão!");
         }
     }
+
+
 }
