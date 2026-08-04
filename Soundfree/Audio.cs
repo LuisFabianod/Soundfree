c811b35d80a266f53f937ae393c7faf1d4d81ed4
Luis Fabiano <luisfabianodev@gmail.com>
2026-08-04 20:53:12 -0300

    Adicionar classe áudio e funcionalidades dos botões play e adicionar

---
+++ Soundfree/Audio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Soundfree
+{
+    public class Audio
+    {
+
+        public string Nome { get; set; }
+
+        public string Caminho { get; set; }
+
+        public TimeSpan Duracao { get; set; }
+
+        public Audio(string nome, string caminho)
+        {
+            Nome = nome;
+            Caminho = caminho;
+        }
+
+        public override string ToString()
+        {
+            return Nome;
+        }
+    }
+}
--- Soundfree/MainWindow.xaml.cs
+++ Soundfree/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+using NAudio.Wave;
 
 namespace Soundfree
 {
@@ -19,11 +22,51 @@ namespace Soundfree
             InitializeComponent();
 
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void BtnAdicionar_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Você clicou no botão!");
+            OpenFileDialog dialog = new OpenFileDialog();
+            
+            dialog.Filter = "Arquivos de Áudio|*.mp3;*.wav";
+            
+            bool? resultado = dialog.ShowDialog();
+            
+            if (resultado == true)
+            {
+                Audio audio = new Audio(System.IO.Path.GetFileName(dialog.FileName), dialog.FileName);
+                
+                audios.Add(audio);
+
+                ListaAudios.Items.Add(audio);
+            }
         }
+
+        private void BtnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListaAudios.SelectedItem == null)
+            {
+                MessageBox.Show("Selecione um áudio.");
+                return;
+            }
+
+            Audio audio = (Audio)ListaAudios.SelectedItem;
+
+            player?.Stop();
+            player?.Dispose();
+            audioFile?.Dispose();
+
+            audioFile = new AudioFileReader(audio.Caminho);
+
+            player = new WaveOutEvent();
+            player.Init(audioFile);
+            player.Play();
+        }
+
+        private List<Audio> audios = new List<Audio>();
+
+        private WaveOutEvent? player;
+        private AudioFileReader? audioFile;
     }
+
 
 
 }
